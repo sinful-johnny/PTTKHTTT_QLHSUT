55d10229a578d695d8259970013b97c395854f65
KyleKennyNelson <147902202+KyleKennyNelson@users.noreply.github.com>
2024-05-07 18:42:06 +0700

    DangKyThanhVien

---
+++ UISourceCode/UI_Prototype/UI_Prototype/GUI/DangKiThanhVien/DangKyThanhVien.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_Prototype.GUI.DangKiThanhVien
+{
+    /// <summary>
+    /// Interaction logic for DangKyThanhVien.xaml
+    /// </summary>
+    public partial class DangKyThanhVien : Window
+    {
+        public DangKyThanhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void KhongHopLeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            DialogResult = false;
+        }
+
+        private async void HopLeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            LoadingProgressBar.IsIndeterminate = false;
+            LoadingProgressBar.Value = 10;
+            await Task.Run(() => Thread.Sleep(10));
+            LoadingProgressBar.Value = 40;
+            await Task.Run(() => Thread.Sleep(25));
+            LoadingProgressBar.Value = 80;
+            await Task.Run(() => Thread.Sleep(50));
+            LoadingProgressBar.Value = 100;
+            await Task.Run(() => Thread.Sleep(25));
+            MessageBox.Show("Sửa thông tin hợp đồng thành công!", "Thành công", MessageBoxButton.OK);
+            DialogResult = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UISourceCode/UI_Prototype/UI_Prototype/GUI/DangKiThanhVien/DanhSachDoanhNghiepDangKy.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI_Prototype.GUI.DangKiThanhVien
+{
+    /// <summary>
+    /// Interaction logic for DanhSachDoanhNghiepDangKy.xaml
+    /// </summary>
+    public partial class DanhSachDoanhNghiepDangKy : Window
+    {
+        public DanhSachDoanhNghiepDangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HienThiDNButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void XoaDNButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TTDoanhNghiepDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
